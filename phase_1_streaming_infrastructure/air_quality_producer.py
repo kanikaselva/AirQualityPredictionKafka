2f1ad6608dbd5fdd6801810504fc0d2faf308c66
Kanika Selvapandian <kselvapa@andrew.cmu.edu>
2025-09-16 17:24:39 -0400

    installed batching

--- phase_1_streaming_infrastructure/air_quality_producer.py
+++ phase_1_streaming_infrastructure/air_quality_producer.py
@@ -3,6 +3,7 @@ import pandas as pd
 import time
 from kafka import KafkaProducer
 import json
+import logging
   
 # fetch dataset 
 air_quality = fetch_ucirepo(id=360) 
@@ -20,12 +21,30 @@ producer = KafkaProducer(
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-# Stream each row
+# Simulate streaming with batching
+batch_size = 20   # send 20 rows per simulated "batch"
+interval = 1      # delay between sends (1 second)
+
+
+buffer = []
+
 for index, row in df.iterrows():
     data = row.to_dict()
-    producer.send('air_quality', value=data)
-    print(f"Sent: {data}")
-    time.sleep(1)  # simulate real-time streaming
+    buffer.append(data)
+
+    # Once buffer is full, flush batch
+    if len(buffer) >= batch_size:
+        for record in buffer:
+            producer.send("air_quality", value=record)
+        logging.info(f"Sent batch of {len(buffer)} records")
+        buffer.clear()
+        time.sleep(interval)  # simulate real-time delay
+
+# Send leftover records if any
+if buffer:
+    for record in buffer:
+        producer.send("air_quality", value=record)
+    logging.info(f"Sent final batch of {len(buffer)} records")
 
 producer.flush()
 producer.close()
